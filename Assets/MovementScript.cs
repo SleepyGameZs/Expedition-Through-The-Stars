53a7e045c2f370b8b356e859df39258082f1d5ce
Justin <justintoeat@gmail.com>
2022-01-15 14:00:06 -0500

    Just movement Script
    its the literally just the movement script

---
+++ Assets/MovementScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class MovementScript : MonoBehaviour
+{
+    Rigidbody2D RBody2D;
+    [SerializeField] float speed = 2f;
+    Vector2 motionVector;
+
+    void Awake()
+    {
+        RBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        motionVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        RBody2D.velocity = motionVector * speed;
+    }
+}
